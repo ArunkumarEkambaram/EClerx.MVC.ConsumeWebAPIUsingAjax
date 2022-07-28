77730c1f3a2c94af5f45d646808724cc00fe453f
Arunkumar <arunkumar.e@outlook.com>
2022-07-28 13:54:13 +0530

    Add project files.

---
+++ EClerx.MVC.ConsumeWebAPIUsingAjax/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EClerx.MVC.ConsumeWebAPIUsingAjax.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
